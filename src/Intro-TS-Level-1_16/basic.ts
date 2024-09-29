1bbe49f591e87e648820b9b74c12cb8960a03adc
Neco_Stack <necoabay@googlemail.com>
2024-09-29 18:23:19 +0200

    operators

---
+++ src/Intro-TS-Level-1_16/basic.ts
@@ -0,0 +1,27 @@
+// Multiplikation geht vor
+// let score:number = 5 + 5 * 10;
+// console.log('Ergebnis: ' + score);
+
+// mit Klammern wird Reihenfolge der Prioritäten verändert 
+// let score = (5 + 5) * 10;
+// console.log('Ergebnis: ' + score);
+
+// Werte auf Variablen addieren:
+let score = 0; 
+score = score + 10;
+console.log('Ergebnis: ' + score);
+
+score += 10; 
+console.log('Ergebnis: ' + score);
+
+// Inkrementierungen und Dekrementierungen
+let zahl = 1; 
+console.log(zahl);
+zahl ++; 
+console.log(zahl);
+zahl--;
+console.log('decrement: ' + zahl);
+
+
+
+
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+   <a href='./src/Intro-TS-Level-1_16/basic.html'>Intro TS</a>
+  </div>
+`
+
+
